d137ab54415a19c7c54897ba1fe9e343b79ca180
PAOK-2001 <2001.paok@gmail.com>
2022-04-11 22:32:15 -0500

    Added background changes capabilities

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { useEffect, useState } from "react";
+import backgrounds from "./backgroundArray";
 import './App.css';
 import logo from './mlh-prep.png'
 
 function App() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
-  const [city, setCity] = useState("New York City")
+  const [city, setCity] = useState("New York City");
   const [results, setResults] = useState(null);
-
+  const [cardBackground, setcardBackground] = useState("Clear");
   useEffect(() => {
     fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric" + "&appid=" + process.env.REACT_APP_APIKEY)
       .then(res => res.json())
@@ -18,6 +19,7 @@ function App() {
           } else {
             setIsLoaded(true);
             setResults(result);
+            setcardBackground(result.weather[0].main)
           }
         },
         (error) => {
@@ -29,7 +31,8 @@ function App() {
 
   if (error) {
     return <div>Error: {error.message}</div>;
-  } else {
+  } 
+  else {
     return <>
       <img className="logo" src={logo} alt="MLH Prep Logo"></img>
       <div>
@@ -38,17 +41,28 @@ function App() {
           type="text"
           value={city}
           onChange={event => setCity(event.target.value)} />
+
+
         <div className="Results">
           {!isLoaded && <h2>Loading...</h2>}
           {console.log(results)}
           {isLoaded && results && <>
-            <h3>{results.weather[0].main}</h3>
-            <p>Feels like {results.main.feels_like}°C</p>
-            <i><p>{results.name}, {results.sys.country}</p></i>
+            <div className="WeatherCard" style= {{backgroundImage: `url(${backgrounds[cardBackground]})`}}>
+              <h3>{results.weather[0].main}</h3>
+              <p>Feels like {results.main.feels_like}°C</p>
+              <i><p>{results.name}, {results.sys.country}</p></i>
+            </div>
+
+            <div className="Map">
+              <h3>{results.weather[0].main}</h3>
+              <p>Feels like {results.main.feels_like}°C</p>
+              <i><p>{results.name}, {results.sys.country}</p></i>
+            </div>
           </>}
         </div>
       </div>
     </>
+
   }
 }
 
---
+++ src/backgroundArray.js
@@ -0,0 +1,8 @@
+import clearBack from './Clear.jpg';
+import rainBack  from './Rainy.jpeg';
+import hazeBack  from './Haze.jpeg';
+import cloudBack from './Cloud.jpg'
+
+const backgrounds = {Clear: clearBack, Rain: rainBack , Haze:hazeBack, Clouds: cloudBack};
+
+export default backgrounds;
