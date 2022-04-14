0bbb64d425f863c4b022cf2c2c9d12e63abb997c
suraj-singh127 <surajsingh25163@gmail.com>
2022-04-14 22:08:38 +0530

    added navbar component

---
+++ src/components/Navbar/navbar.js
@@ -0,0 +1,16 @@
+import './navbar.css';
+
+function Navbar(props) {
+  const { src } = props;
+  return (
+    <nav className="navbar">
+      <img className="logo" src={src} alt="MLH Prep Logo" />
+      <ul className="navlist">
+        <li className="navlist-link">Weather News</li>
+        <li className="navlist-link">Tell Me Weather</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
