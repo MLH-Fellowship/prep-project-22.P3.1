45173aaabb7a3dc3921944dbd94e7953c6ecdc61
Suraj Singh <84654095+suraj-singh127@users.noreply.github.com>
2022-04-17 02:03:01 +0530

    Geolocation integrated (#39)
    * added all the files
    
    * Made weathear card a component
    
    * ESLINT corrections
    
    * Changed styling of search bar
    
    * Changed search prompt
    
    * add custom hook for user's location
    
    * setup the new environment
    
    * feature: added custom hook to extract geolocation
    
    * fix: minor issue
    
    * feature: added custom hook to fetch city name from lat and longitude
    
    * fix: triggering location based weather on start
    
    * fetch the current city from OpenWeatherMapApi
    
    * remove unnecessary console logs
    
    * geolocation feature added
    
    * geolocation integrated succesfully
    
    * this is annoying .eslintcache deleted
    
    Co-authored-by: PAOK-2001 <2001.paok@gmail.com>
    Co-authored-by: ankitaugale23 <ankitaugale23@gmail.com>
    Co-authored-by: JyotimoyKashyap <jyotimoykashyap123@gmail.com>

---
+++ src/hooks/useFetchCity.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-unused-vars */
+import { useState, useEffect } from 'react';
+
+const useFetchCity = (lat, lng) => {
+  const [geoCity, setGeoCity] = useState({
+    city: '',
+    countryCode: '',
+  });
+
+  const [error, setError] = useState(null);
+  const [isLoaded, setLoaded] = useState(false);
+
+  // eslint-disable-next-line no-use-before-define
+  useEffect(() => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${process.env.REACT_APP_APIKEY}`;
+
+    fetch(url)
+      .then((res) => res.json())
+      .then((result) => {
+        if (result) {
+          setGeoCity({
+            city: result.name,
+            countryCode: result.sys.country,
+          });
+        }
+        setLoaded(true);
+      })
+      .catch((err) => {
+        setError(err);
+      });
+  }, [lat, lng]);
+
+  return geoCity;
+};
+
+export default useFetchCity;
