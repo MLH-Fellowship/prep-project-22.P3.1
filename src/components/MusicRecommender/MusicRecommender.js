ed394987234cbfe45174cc5c11108c80e0094e07
mutugiii <mutugimutuma@gmail.com>
2022-04-19 14:32:17 +0300

    Add workflow for users to select their preferred song choices && Managing the selection of choices based on preference and weather && using localstorage to store userpreference && Fix linting issues

--- src/components/MusicRecommender/MusicRecommender.js
+++ src/components/MusicRecommender/MusicRecommender.js
@@ -1,49 +1,66 @@
-/* eslint-disable */
-import { useState, useEffect } from "react";
-import "./MusicRecommender.css"
+import { useState, useEffect } from 'react';
+import './MusicRecommender.css';
 
-import SpotifyComponent from "./SpotifyComponent";
+import SpotifyComponent from './SpotifyComponent';
+import SongChoices from './SongChoices';
 
 const MusicRecommender = ({ props }) => {
-  const [playlistId, setPlaylistId] = useState("7LqjQuTFvBj2TFq5kq9mCp");
+  const [playlistId, setPlaylistId] = useState(null);
   const [userPreference, setUserPreferences] = useState(null);
-  // Stop music when new audio item is clicked - SpotifyComponent
-  // Select the playlist based on the preference and the weather
-  // Apple music API & embed
-  // Login with spotify & Apple music
 
-  // Update the preference and playlistId
-  useEffect(() => setPlaylistId("78ENPoHD87U9kXWc9VWnJU"), [playlistId])
+  // Select the playlist based on weather codition and preference
+  useEffect(() => {
+    try {
+      if (userPreference)
+        // eslint-disable-next-line no-unused-expressions
+        setPlaylistId(SongChoices[props.weather[0].main][userPreference]);
+    } catch (err) {
+      console.error(err);
+    }
+  }, [userPreference, props, playlistId]);
 
+  // Get the preference saved from localstorage and update the userPreference variable
+  useEffect(
+    () => setUserPreferences(localStorage.getItem('preference')),
+    [userPreference]
+  );
 
-  useEffect(() => setUserPreferences(localStorage.getItem('preference')), [userPreference]);
-
-  const handlePreference = preference => {
+  // Function to save preference to localstorage
+  const handlePreference = (preference) => {
     if (preference != null) {
       localStorage.setItem('preference', preference);
       setUserPreferences(preference);
     }
-  }
+  };
 
   return (
     <>
       <div className="container">
+        <br />
+        <h3>Your Music Forecast!!</h3>
 
-        <br /><h3>Your Music Forecast!!</h3>
-
-        {userPreference == null ?
+        {userPreference == null ? (
+          // Render the UI to select a preference if the preference has not been set
           <>
             <br />
-            <b><i>
-              <h4>Looking to get your <span className="goldenWords">Music Forecast</span> ?</h4>
-              <h5>Select your favorite genre!</h5>
-            </i></b>
+            <b>
+              <i>
+                <h4>
+                  Looking to get your{' '}
+                  <span className="golden_words">Music Forecast</span> ?
+                </h4>
+                <h5>Select your favorite genre!</h5>
+              </i>
+            </b>
             <div className="row justify-content-center">
               <div className="card text-center bg-dark w-50 col-md">
                 <div className="card-body">
                   <h5 className="card-title">Pop</h5>
-                  <button type="button" className="btn btn-block btn-light"
-                    onClick={() => handlePreference("pop")}>
+                  <button
+                    type="button"
+                    className="btn btn-block btn-light"
+                    onClick={() => handlePreference('pop')}
+                  >
                     Confirm
                   </button>
                 </div>
@@ -51,8 +68,11 @@ const MusicRecommender = ({ props }) => {
               <div className="card text-center bg-dark w-50 col-md">
                 <div className="card-body">
                   <h5 className="card-title">Hip Hop and Rap</h5>
-                  <button type="button" className="btn btn-block btn-light"
-                    onClick={() => handlePreference("rap")}>
+                  <button
+                    type="button"
+                    className="btn btn-block btn-light"
+                    onClick={() => handlePreference('rap')}
+                  >
                     Confirm
                   </button>
                 </div>
@@ -60,23 +80,28 @@ const MusicRecommender = ({ props }) => {
               <div className="card text-center bg-dark w-50 col-md">
                 <div className="card-body">
                   <h5 className="card-title">Electric Beats and House</h5>
-                  <button type="button" className="btn btn-block btn-light"
-                    onClick={() => handlePreference("house")}>
+                  <button
+                    type="button"
+                    className="btn btn-block btn-light"
+                    onClick={() => handlePreference('house')}
+                  >
                     Confirm
                   </button>
                 </div>
               </div>
             </div>
+            <br />
+            <br />
           </>
-          :
+        ) : (
+          // Render the Spotify component if the user preference is set!
           <>
             <SpotifyComponent props={props} playlistId={playlistId} />
           </>
-        }
-
+        )}
       </div>
     </>
-  )
+  );
 };
 
 export default MusicRecommender;
