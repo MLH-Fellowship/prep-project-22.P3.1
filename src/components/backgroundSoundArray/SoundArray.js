5c46408f523a3c0b1fb9afe51b6013d0ebe042c9
abhishekrawe <rajputabhisheksingh53@gmail.com>
2022-04-19 22:47:24 +0530

    added a new Component Background sound array

---
+++ src/components/backgroundSoundArray/SoundArray.js
@@ -0,0 +1,14 @@
+import morningBird from './assets/morningbird.mp3'
+import rainDrop from './assets/raindrop.mp3'
+import sunriseBird from './assets/sunrisebird.mp3'
+import thunder from './assets/thunder.mp3'
+import windStorm from './assets/windStorm.mp3'
+
+const sounds = {
+    morning : morningBird,
+    Rain: rainDrop,
+    sunrise: sunriseBird,
+    thunder: thunder,
+    wind : windStorm
+}
+export default sounds;
