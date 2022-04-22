9ff71b8ed7f335a7ca8b6b69b0024c52b955d2cc
abhishekrawe <rajputabhisheksingh53@gmail.com>
2022-04-22 20:40:03 +0530

    move audio files to assets

--- src/components/backgroundSoundArray/SoundArray.js
+++ src/components/backgroundSoundArray/SoundArray.js
@@ -1,8 +1,8 @@
-import morningBird from './assets/morningbird.mp3';
-import rainDrop from './assets/raindrop.mp3';
-import sunriseBird from './assets/sunrisebird.mp3';
-import thunder from './assets/thunder.mp3';
-import windStorm from './assets/windStorm.mp3';
+import morningBird from '../../assets/morningbird.mp3';
+import rainDrop from '../../assets/raindrop.mp3';
+import sunriseBird from '../../assets/sunrisebird.mp3';
+import thunder from '../../assets/thunder.mp3';
+import windStorm from '../../assets/windStorm.mp3';
 
 const sounds = {
   morning: morningBird,
